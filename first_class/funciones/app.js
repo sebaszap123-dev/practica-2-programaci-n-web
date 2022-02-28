845c1c95da418529378fb8b05fe27d376b854cd3
sebaszap123-dev <freeway.dev.sf@gmail.com>
2022-02-27 23:24:58 -0600

    practica 3 Modules

--- first_class/funciones/app.js
+++ first_class/funciones/app.js
@@ -1,15 +1,21 @@
-greet = () => {
+let greet = () => {
   console.log("Hello world");
 };
 
 greet();
 
-logGreeting = (fn) => {
-  fn();
-};
+let logGreeting = (miParametro) => miParametro();
 logGreeting(greet);
 
-greetMe = (greet) => {
+let greetMe = (greet) => {
   console.log(greet);
 };
 greetMe("Hello from the function expression");
+
+// interpolación de string "clasica"
+let logGreeting1 = (miNombre, miColor) => {
+  console.log(
+    "Hola " + miNombre + " buenos días! Tu color favorito es el " + miColor
+  );
+};
+logGreeting1("Miguel", "azul");
